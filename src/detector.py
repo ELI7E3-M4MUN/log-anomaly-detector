c9bb606702c1e452a2f574a801b4be53c000e08f
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-03 16:45:34 +0300

    Update detector.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- src/detector.py
+++ src/detector.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+class Detector:
+    def detect(self, model, df):
+        features = df[["length", "digits", "uppercase"]]
+        scores = model.decision_function(features)
+        df["anomaly_score"] = scores
+        df["is_anomaly"] = scores < np.percentile(scores, 5)
+        return df
