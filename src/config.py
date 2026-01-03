fde358c38a77ecf747ac41fa6057b4487ff3c479
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-03 16:44:14 +0300

    Update config.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- src/config.py
+++ src/config.py
@@ -1,1 +1,13 @@
+LOG_SOURCES = [
+    "data/sample_logs/syslog.log",
+    "data/sample_logs/auth.log",
+    "data/sample_logs/nginx.log"
+]
 
+MODEL_PATHS = {
+    "isolation_forest": "models/isolation_forest.pkl",
+    "lof": "models/lof.pkl",
+    "ocsvm": "models/oneclass_svm.pkl"
+}
+
+PROCESSED_DIR = "data/processed/"
