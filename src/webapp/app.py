e93716db334e407d5399f8baebc888c920467deb
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-03 16:46:48 +0300

    Update app.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+import pandas as pd
 
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+    df = pd.read_json("data/processed/output.json")
+    anomalies = df[df["is_anomaly"] == True]
+    return render_template("dashboard.html", anomalies=anomalies.to_dict(orient="records"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
