acf2306c75fd458b711a26e8dbb8b73d8cb0c494
The Secwexen <129677392+secwexen@users.noreply.github.com>
2026-01-03 16:45:50 +0300

    Update reporter.py
    Signed-off-by: The Secwexen <129677392+secwexen@users.noreply.github.com>

--- src/reporter.py
+++ src/reporter.py
@@ -1,1 +1,7 @@
+import json
+import os
 
+class Reporter:
+    def save(self, df, output_path):
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        df.to_json(output_path, orient="records", indent=2)
